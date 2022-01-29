de16173c14a11e3609c3f87269320091026fba44
amandaamabili <amanda_amabilly.b@hotmail.com>
2022-01-29 16:48:33 -0300

    add modules FinancialData and import

--- routes/index.ts
+++ routes/index.ts
@@ -2,11 +2,13 @@ import {Router} from 'express';
 
 
 import { userRoutes, usersRoutes } from './user.routes';
+import {financialRoutes} from './financialData.routes';
 
 const router = Router();
 
 
 router.use("/user",userRoutes);
-router.use("/users", usersRoutes)
+router.use("/users", usersRoutes);
+router.use("/financial", financialRoutes );
 
 export {router};
